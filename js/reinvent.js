da5fb58494f4a9a34ac5bac8c0accd78543a6779
Jhondee Gorillo <jhondeeegorillo@gmail.com>
2026-07-01 12:00:04 +0800

    Add files via upload

--- js/reinvent.js
+++ js/reinvent.js
@@ -9,3 +9,21 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+const codeText = `10.10.10.1`;
+const copyCodeBtn = document.getElementById("copy-code-btn");
+const copyCodeBlockBtn = document.querySelector(".code-block button");
+
+copyCodeBtn.onclick = () => {
+  navigator.clipboard
+    .writeText(codeText)
+    .then(() => {
+      copyCodeBtn.innerHTML = `Copied`;
+      setTimeout(() => (copyCodeBtn.innerHTML = `Copy Portal Link`), 2000);
+    })
+    .catch(() => {
+      copyCodeBtn.innerHTML = `Failed`;
+      setTimeout(() => (copyCodeBtn.innerHTML = `Copy Portal Link`), 2000);
+    });
+};
+
